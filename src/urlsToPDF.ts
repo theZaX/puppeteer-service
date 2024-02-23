74b54d8ed35fc9d616ee34da29583b7c28b03b7e
Zachary Allen <zacharyallen654321@gmail.com>
2024-02-23 15:51:40 -0700

    gave it more time but more parallel

--- src/urlsToPDF.ts
+++ src/urlsToPDF.ts
@@ -6,7 +6,7 @@ import { PDFDocument } from "pdf-lib";
 export const smartURLToPDF = async (url: string, page: Page) => {
   await page.goto(url, {
     waitUntil: "networkidle2",
-    timeout: 8000, // 10 seconds
+    timeout: 10000, // 10 seconds
   });
 
   let height = await page.evaluate(() => document.body.scrollHeight);
@@ -56,8 +56,8 @@ const mergePDFs = async (pdfs: Buffer[]) => {
 export const urlsToPDF = async (urls: string[], storageKey: string) => {
   const data = await inBrowser(async (browser) => {
     const buffers = [];
-    for (let i = 0; i < urls.length; i += 10) {
-      const chunk = urls.slice(i, i + 10);
+    for (let i = 0; i < urls.length; i += 15) {
+      const chunk = urls.slice(i, i + 15);
       const pdfPromises = chunk.map(async (url) => {
         const page = await browser.newPage();
         await page.setViewport({ width: 1066, height: 700, isLandscape: true });
